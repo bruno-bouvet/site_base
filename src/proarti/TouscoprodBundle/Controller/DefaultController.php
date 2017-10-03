0eab22a95ead471dd9ac0160522ba05a32608d61
bruno-bouvet <bouvet.bruno@gmail.com>
2017-10-03 11:03:18 +0200

    tweaked the nav and the carousel added faq and explanation pages

--- src/proarti/TouscoprodBundle/Controller/DefaultController.php
+++ src/proarti/TouscoprodBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace proarti\TouscoprodBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -12,6 +13,47 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('proartiTouscoprodBundle:Default:index.html.twig');
+        return $this->render('@proartiTouscoprod/Default/index.html.twig');
     }
+
+    /**
+     * @Route("/faq")
+     *
+     * @return Response
+     */
+    public function faqAction()
+    {
+        return $this->render('@proartiTouscoprod/Default/faq.html.twig');
+    }
+
+    /**
+     * @Route("/explanation")
+     *
+     * @return Response
+     */
+    public function explanationAction()
+    {
+        return $this->render('@proartiTouscoprod/Default/explanation.html.twig');
+    }
+
+    /**
+     * @Route("/collect-default")
+     *
+     * @return Response
+     */
+    public function collectDefaultAction()
+    {
+        return $this->render('@proartiTouscoprod/Default/collect-default.html.twig');
+    }
+
+    /**
+     * @Route("/collect")
+     *
+     * @return Response
+     */
+    public function collectPageAction()
+    {
+        return $this->render('@proartiTouscoprod/Default/collect-pages/collect-1.html.twig');
+    }
+
 }
